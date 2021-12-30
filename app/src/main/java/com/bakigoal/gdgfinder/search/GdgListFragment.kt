365958a1afef28f425bb2876a5622f729661a88b
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-30 11:40:53 +0500

    add chipGroup

--- app/src/main/java/com/bakigoal/gdgfinder/search/GdgListFragment.kt
+++ app/src/main/java/com/bakigoal/gdgfinder/search/GdgListFragment.kt
@@ -9,10 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bakigoal.gdgfinder.R
 import com.bakigoal.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 private const val LOCATION_PERMISSION_REQUEST = 1
@@ -34,7 +35,7 @@ class GdgListFragment : Fragment() {
         val binding = FragmentGdgListBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = viewLifecycleOwner
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
@@ -47,16 +48,34 @@ class GdgListFragment : Fragment() {
         // Sets the adapter of the RecyclerView
         binding.gdgChapterList.adapter = adapter
 
-        viewModel.showNeedLocation.observe(viewLifecycleOwner, object : Observer<Boolean> {
-            override fun onChanged(show: Boolean?) {
-                // Snackbar is like Toast but it lets us show forever
-                if (show == true) {
-                    Snackbar.make(
-                        binding.root,
-                        "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
-                        Snackbar.LENGTH_LONG
-                    ).show()
+        viewModel.showNeedLocation.observe(viewLifecycleOwner, { show ->
+            // Snackbar is like Toast but it lets us show forever
+            if (show == true) {
+                Snackbar.make(
+                    binding.root,
+                    "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, {
+            it?.apply {
+                val chipGroup = binding.regionsList
+                val inflater = LayoutInflater.from(chipGroup.context)
+
+                val children = it.map { regionName ->
+                    val chip = inflater.inflate(R.layout.region ,chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener{button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
                 }
+
+                chipGroup.removeAllViews()
+                children.forEach(chipGroup::addView)
             }
         })
 
